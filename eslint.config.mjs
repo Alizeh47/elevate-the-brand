b0e5aea179375254539adc2e8247eeca1ab744cd
Alizeh47 <zephyrsifaci09@gmail.com>
2025-02-21 00:38:03 +0500

    file structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'prettier',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:tailwindcss/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'tailwindcss'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  }),
 ];
-
-export default eslintConfig;
